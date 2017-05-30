20d011708d54880e5f3b8133576c421196caa0cf
Matthias Ekman <matthias.ekman@gmail.com>
2017-05-30 15:05:04 +0200

    FIX: PEP compl

--- mansfield/tests/test_mansfield.py
+++ mansfield/tests/test_mansfield.py
@@ -1,5 +1,5 @@
-from __future__ import absolute_import, division, print_function
-import os.path as op
-import numpy as np
-import numpy.testing as npt
-import mansfield as ma
+# from __future__ import absolute_import, division, print_function
+# import os.path as op
+# import numpy as np
+# import numpy.testing as npt
+# import mansfield as ma
